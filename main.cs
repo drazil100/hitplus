c024053867c0613fef23fc7166ad949f48387025
Austin Allman <drazil100@gmail.com>
2017-09-12 08:59:02 -0500

    Added rough fuctionality

--- main.cs
+++ main.cs
@@ -6,10 +6,28 @@ using System.Windows.Forms;
 using System.Drawing;
 using System.Runtime.InteropServices;
 
+public class Entry
+{
+	public string name = "";
+	public int score = 0;
+	
+	public Entry(string name, int score)
+	{
+		this.name = name;
+		this.score = score;
+	}
+	
+	public override string ToString()
+	{
+		return String.Format("{0}:{1}", name, score);
+	}
+}
+
 public class Score : Panel
 {
 	private static Dictionary<string, Score> scores = new Dictionary<string, Score>();
 	public static List<Score> scoresList = new List<Score>();
+	public static List<Entry> topData = new List<Entry>();
 	
 	private Label nameLabel = new Label();
 	private Label scoreLabel = new Label();
@@ -29,6 +47,11 @@ public class Score : Panel
 	private int index = 0;
 	public int arrayIndex = 0;
 	
+	public override string ToString()
+	{
+		return String.Format("{0}:{1}", name, CurrentScore);
+	}
+	
 	public int CurrentScore {
 		get {return score;}
 		set 
@@ -36,19 +59,19 @@ public class Score : Panel
 			score = value;
 			int runScore = 0;
 			int oldScore = 0;
-			for (int i = 0; i <= index; i++)
+			for (int i = 0; (i < scoresList.Count && i <= index); i++)
 			{
 				oldScore += scoresList[i].pbScore;
 				runScore += scoresList[i].CurrentScore;
 			}
 			if (runScore >= oldScore)
 			{
-				scoreLabel.Text = String.Format("{0}: +{1}, ", name, (runScore - oldScore));
+				scoreLabel.Text = String.Format("{0}\n+{1}", score, (runScore - oldScore));
 				scoreLabel.ForeColor = ahead;
 			}
 			else
 			{
-				scoreLabel.Text = String.Format("{0}: -{1}, ", name, (oldScore - runScore));
+				scoreLabel.Text = String.Format("{0}\n-{1}", score, (oldScore - runScore));
 				scoreLabel.ForeColor = behind;
 			}
 			if (score > best)
@@ -59,8 +82,128 @@ public class Score : Panel
 			if (score == 0)
 			{
 				scoreLabel.ForeColor = text_default;
-				scoreLabel.Text = String.Format(" {0}", pbScore);
+				scoreLabel.Text = ""+pbScore;
 				
+			}
+		}
+	}
+	
+	public static void UpdateBestScores()
+	{
+		bool updated = false;
+		for(int i = 0; i < scoresList.Count; i++)
+		{
+			if (scoresList[i].CurrentScore > scoresList[i].best)
+			{
+				scoresList[i].best = scoresList[i].CurrentScore;
+				updated = true;
+				foreach(Entry entry in topData)
+				{
+					if (entry.name == scoresList[i].name)
+					{
+						entry.score = scoresList[i].CurrentScore;
+					}
+				}
+			}
+		}
+		
+		if (updated)
+		{
+			string temp = topData[0].ToString();
+		
+			for(int i = 1; i < topData.Count; i++)
+			{
+				temp += "\n" + topData[i].ToString();
+			}
+			
+			try
+			{
+				string file = @"pb_individuals.txt";
+				var sw = new StreamWriter(file);
+				sw.Write(temp);
+				sw.Close();
+			}
+			catch (Exception e2)
+			{
+				Console.WriteLine("Error: " + e2.Message);
+			}
+		}
+	}
+	
+	public static void SaveRun()
+	{
+		
+		string temp = "";
+		int sob = 0;
+		int tot = GetTotal();
+		bool doSave = false;
+		
+		if (GetTotal() > GetOldScore())
+			doSave = true;
+		
+		try
+		{
+			temp = scoresList[0].ToString();
+			
+			for(int i = 0; i < scoresList.Count; i++)
+			{
+				if (i > 0)
+					temp += "\n" + scoresList[i].ToString();
+				
+				sob += scoresList[i].best;
+				if (doSave)
+					scoresList[i].pbScore = scoresList[i].CurrentScore;
+				scoresList[i].CurrentScore = 0;
+			}
+		}
+		catch (Exception e3)
+		{
+			Console.WriteLine("Error: " + e3.Message);
+			return;
+		}
+		
+		ScoreWindow.sobScore.Text = "SoB: " + sob;
+		string config = "";
+		
+		if (!doSave)
+			return;
+		
+		ScoreWindow.topScore.Text = "Top: " + tot;
+		
+		try
+		{
+			config = File.ReadAllText(@"config.txt", Encoding.UTF8);
+			
+			try
+			{
+				string file = "";
+				
+				if (config == "Hard Route: 0")
+					file = @"pb_easy.txt";
+				else
+					file = @"pb_hard.txt";
+				
+				var sw = new StreamWriter(file);
+				sw.Write(temp);
+				sw.Close();
+			}
+			catch (Exception e2)
+			{
+				Console.WriteLine("Error: " + e2.Message);
+			}
+			
+		}
+		catch (Exception)
+		{
+			try
+			{
+				var sw = new StreamWriter(@"emergency_save.txt");
+				sw.Write(temp);
+				sw.Close();
+			}
+			catch (Exception e2)
+			{
+				Console.WriteLine("Error: " + e2.Message);
 			}
 		}
 	}
@@ -79,11 +222,11 @@ public class Score : Panel
 		scoresList.Add(this);
 		nameLabel.ForeColor = text_default;
 		if (index == 0)
-			nameLabel.ForeColor = current;
+			Highlight();
 		scoreLabel.ForeColor = text_default;
 		this.name = name;
 		this.pbScore = score;
-		scoreLabel.Text = String.Format(" {0}", pbScore);
+		scoreLabel.Text = String.Format("{0}", pbScore);
 		nameLabel.Text = String.Format("{0}:", name);
 		
 		Controls.Add(nameLabel);
@@ -117,6 +260,37 @@ public class Score : Panel
 			if (arrayIndex > -1)
 				scores[name].arrayIndex = arrayIndex;
 		}
+		topData.Add(new Entry(name, score));
+	}
+	
+	public static int GetTotal()
+	{
+		int t = 0;
+		foreach (Score s in scoresList)
+		{
+			t += s.CurrentScore;
+		}
+		return t;
+	}
+	
+	public static int GetOldScore()
+	{
+		int t = 0;
+		foreach (Score s in scoresList)
+		{
+			t += s.pbScore;
+		}
+		return t;
+	}
+	
+	public void Highlight()
+	{
+		nameLabel.ForeColor = current;
+	}
+	
+	public void Unhighlight()
+	{
+		nameLabel.ForeColor = text_default;
 	}
 	
 	private void DoLayout()
@@ -128,6 +302,166 @@ public class Score : Panel
 		scoreLabel.Width = Width - 60;
 	}
 	
+}
+
+public class ScoreInput : Form
+{
+	private TextBox inputBox = new TextBox();
+	private Button submit = new Button();
+	private Button undo = new Button();
+	private Button save = new Button();
+	
+	public int index = 0;
+	
+	public ScoreInput()
+	{
+		Size = new Size(300, 200);
+		
+		submit.Text = "Keep";
+		undo.Text = "Back";
+		undo.Enabled = false;
+		save.Text = "Save & Reset";
+		save.Enabled = false;
+	
+	
+		submit.Click += new EventHandler(OnSubmit);
+		undo.Click += new EventHandler(OnUndo);
+		save.Click += new EventHandler(OnReset);
+		
+		SwapControls(submit);
+		
+		//Resize += delegate { DoLayout(); };
+		
+	}
+	
+	private int GetWidth()
+	{
+		return (
+				Width - (2 * SystemInformation.FrameBorderSize.Width)
+		       );
+	}
+
+	private int GetHeight()
+	{
+		return (
+				Height - (2 * SystemInformation.FrameBorderSize.Height +
+					SystemInformation.CaptionHeight)
+		       );
+	}
+	
+	private void DoLayout()
+	{
+		inputBox.Height = 25;
+		inputBox.Width = GetWidth();
+		submit.Top = 25;
+		submit.Width = GetWidth()/2;
+		submit.Height = 25;
+		undo.Top = submit.Top;
+		undo.Width = GetWidth() - submit.Width;
+		undo.Left = submit.Width;
+		save.Top = submit.Top;
+		save.Width = submit.Width;
+		save.Height = submit.Height;
+	}
+	
+	public void SwapControls(Control b)
+	{
+		Controls.Clear();
+		
+		Controls.Add(inputBox);
+		Controls.Add(b);
+		Controls.Add(undo);
+		
+		DoLayout();
+	}
+	
+	
+	
+	public void OnSubmit(object sender, EventArgs e)
+	{
+		try
+		{
+			Score.scoresList[index].CurrentScore = Int32.Parse(inputBox.Text);
+			inputBox.Text = "";
+			Score.scoresList[index].Unhighlight();
+			index++;
+			if (index != Score.scoresList.Count)
+				Score.scoresList[index].Highlight();
+			
+			if (index > 0)
+			{
+				undo.Enabled = true;
+			}
+			
+			if (index == Score.scoresList.Count)
+			{
+				submit.Enabled = false;
+				save.Enabled = true;
+				SwapControls(save);
+				return;
+			}
+			else
+			{
+				submit.Enabled = true;
+			}
+		}
+		catch (Exception)
+		{
+			
+		}
+	}
+	
+	
+	public void OnUndo(object sender, EventArgs e)
+	{
+		try
+		{
+			if (index < Score.scoresList.Count)
+			{
+				Score.scoresList[index].CurrentScore = 0;
+				Score.scoresList[index].Unhighlight();
+			}
+			else
+			{
+				SwapControls(submit);
+				save.Enabled = false;
+			}
+			index--;
+			Score.scoresList[index].CurrentScore = 0;
+			Score.scoresList[index].Highlight();
+			inputBox.Text = "" + Score.scoresList[index].CurrentScore;
+			
+			
+			if (index < Score.scoresList.Count)
+			{
+				submit.Enabled = true;
+			}
+			
+			if (index == 0)
+			{
+				undo.Enabled = false;
+				return;
+			}
+			else
+			{
+				undo.Enabled = true;
+			}
+		}
+		catch (Exception)
+		{
+			
+		}
+	}
+	
+	public void OnReset(object sender, EventArgs e)
+	{
+		Score.UpdateBestScores();
+		Score.SaveRun();
+		SwapControls(submit);
+		submit.Enabled = true;
+		index = 0;
+		Score.scoresList[index].Highlight();
+	}
 }
 
 public class ScoreWindow : Form
@@ -145,8 +479,9 @@ public class ScoreWindow : Form
 	//  Declare and initilize UI elements
 	//private LogBox chatLog = new LogBox(); //  LogBox is my own class that has a method for adding messages and built in thread safety
 	private TextBox inputBox = new TextBox();
-	private Label topScore = new Label();
-	private Label sobScore = new Label();
+	public static Label topScore = new Label();
+	public static Label sobScore = new Label();
+	public ScoreInput sInput = new ScoreInput();
 	
 	//private Button sendButton = new Button();
 	//private TabControl rooms = new TabControl();
@@ -173,6 +508,8 @@ public class ScoreWindow : Form
 		this.individual_levels = individual_levels;
 		
 		Font = new Font("Tahoma", 20);
+		Text = "Star Fox 64 Score Tracker";
+		sInput.Text = "Input";
 
 		Size = new Size(1296, 99);
 
@@ -203,6 +540,7 @@ public class ScoreWindow : Form
 		
 		
 		SetControls();
+		
 
 		//  Display documentation so user knows how to use the program
 		//chatLog.AddMessage("use /connect host port nickname");
@@ -213,7 +551,10 @@ public class ScoreWindow : Form
 
 		//  Draw the form
 		DoLayout();
-
+		
+		sInput.Show();
+		Show();
+		
 		//  When the form is shown set the focus to the input box
 
 		//  Close the network connection when the form is closed
@@ -290,19 +631,19 @@ public class ScoreWindow : Form
 		foreach (Score s in sList)
 		{
 			s.Height = GetHeight();
-			s.Width = 140;
+			s.Width = 135;
 		}
 		
 		for (int i = 1; i < sList.Count; i++)
 		{
-			sList[i].Left = sList[i-1].Left + 140;
+			sList[i].Left = sList[i-1].Left + 135;
 		}
 		
-		topScore.Left = sList[sList.Count - 1].Left + 140;
-		topScore.Width = 140;
+		topScore.Left = sList[sList.Count - 1].Left + 135;
+		topScore.Width = 150;
 		topScore.Height = GetHeight();
-		sobScore.Left = topScore.Left + 140;
-		sobScore.Width = 120;
+		sobScore.Left = topScore.Left + 150;
+		sobScore.Width = 150;
 		sobScore.Height = GetHeight();
 	}
 	
