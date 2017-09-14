5d1b71e3cbc8dbbe67506a171d95113c737b2648
Adam Higerd <adam.higerd@moovweb.com>
2017-09-13 20:44:16 -0700

    let foreach work on FileReader

--- FileReader.cs
+++ FileReader.cs
@@ -3,16 +3,20 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 
-public class FileReader
+public class FileReader : IEnumerable<KeyValuePair<string,string>>
 {
 	private string fileName = "";
 	private List<KeyValuePair<string, string>> content = new List<KeyValuePair<string, string>>();
-	
+
+	public IEnumerator<KeyValuePair<string,string>> GetEnumerator() {
+		return content.GetEnumerator();
+	}
+
 	public List<KeyValuePair<string, string>> Content
 	{
 		get { return content;}
 	}
-	
+
 	private FileReader() : base() {}
 	public FileReader(string file) : base()
 	{
@@ -20,24 +24,24 @@ public class FileReader
 		try
 		{
 			string contents = File.ReadAllText(file, Encoding.UTF8);
-			
+
 			string[] lines = contents.Split(new string[] { "\n" }, StringSplitOptions.None);
-			
+
 			foreach (string line in lines)
 			{
 				string[] parts = line.Split(':');
-				
+
 				this[parts[0]] = parts[1].Trim();
 			}
-			
+
 		}
 		catch (Exception)
 		{
-			
+
 		}
-			
+
 	}
-	
+
 	public string this[string key]
 	{
 		get {
@@ -48,7 +52,7 @@ public class FileReader
 			}
 			return "";
 		}
-		
+
 		set {
 			for (int i = 0; i < content.Count; i++)
 			{
@@ -61,7 +65,7 @@ public class FileReader
 			content.Add(new KeyValuePair<string, string>(key, value));
 		}
 	}
-	
+
 	public void AddNewItem(string key, string value)
 	{
 		for (int i = 0; i < content.Count; i++)
@@ -73,7 +77,7 @@ public class FileReader
 		}
 		content.Add(new KeyValuePair<string, string>(key, value));
 	}
-	
+
 	public void Save()
 	{
 		var sw = new StreamWriter(fileName);
@@ -83,5 +87,5 @@ public class FileReader
 		}
 		sw.Close();
 	}
-	
+
 }
